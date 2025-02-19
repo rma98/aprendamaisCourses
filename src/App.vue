943406fcafdbe453f4c7ac10221e7fbb44b4c154
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-02-19 16:03:05 -0300

    Update Setup inicial do projeto Vite

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <router-view></router-view>
+  <Navbar />
+  <router-view />
+  <Footer />
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
+import Pagina from '../views/Pagina.vue'; // Um único componente para todas as páginas
 
 const routes = [
     { path: '/', name: 'Home', component: HomePage },
+    // Rota dinâmica para cursos e páginas
+    { path: "/ingles/:curso/pag:pagina", component: Pagina, name: "CursoPagina" },
+    // Redirecionamento para Home se a rota não existir
     { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
 
