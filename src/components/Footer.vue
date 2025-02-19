e5e819988db30f48b18d77abd0db04f86528ed7f
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-02-19 14:36:31 -0300

    Add component Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- Footer -->
+    <footer class="footer">
+        <p>© {{ new Date().getFullYear() }} <a href="https://aprendamais.mec.gov.br/" target="_blank">AprendaMais</a> Courses. Todos os direitos reservados.</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+}
+</script>
+
+<style scoped>
+/* ======== Footer ======== */
+.footer {
+    background: #264653;
+    color: white;
+    text-align: center;
+    padding: 2rem;
+    margin-top: 3rem;
+    font-size: 1.4rem;
+}
+
+.footer a {
+    color: var(--text-secondary);
+}
+</style>
