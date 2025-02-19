1de829253e008b020023b08f27d95db0210363be
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-02-19 16:03:46 -0300

    Add view Pagina.vue

---
+++ src/views/Pagina.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Aula de Inglês {{ curso }} - Página {{ pagina }}</h1>
+        <component :is="currentPageComponent" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+// Obtendo os parâmetros da rota
+const curso: string = route.params.curso as string;
+const pagina: string = route.params.pagina as string;
+
+// Definir a página específica com base no curso e na página
+const currentPageComponent = defineAsyncComponent(() => {
+    /* @vite-ignore */
+    return import(`./views/Ingles${curso}/Pag${pagina}.vue`);
+});
+</script>
+
+<style scoped></style>
