bb4de8285d34245b637f0cee2ce0a727f27b334c
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-02-28 01:59:51 -0300

    Update Setup inicial do projeto Vite

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import Pagina from '../views/Pagina.vue';
+import Parabens from '../views/Parabens.vue';
 
 const routes = [
     { path: '/', name: 'Home', component: HomePage },
+    { path: '/parabens', name: 'Parabens', component: Parabens },
     // Rota dinâmica para cursos e páginas
     { path: "/ingles/:curso/pag/:pagina", component: Pagina, name: "CursoPagina" },
     { path: '/:pathMatch(.*)*', redirect: '/' }
